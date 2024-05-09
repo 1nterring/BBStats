95dffd645f71ba84062db88987c61b2df65449f5
Wisnih <97796360+Wisnih@users.noreply.github.com>
2024-05-09 22:28:09 +0200

    Update Player.java
    Stworzenie getterów i setterów dla Playera

--- BBStats/src/main/java/com/uep/wap/model/Player.java
+++ BBStats/src/main/java/com/uep/wap/model/Player.java
@@ -41,4 +41,52 @@ public class Player {
     private Float salary;
 
     // Getters and setters
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Timestamp getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Timestamp dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Float weight) {
+        this.weight = weight;
+    }
+
+    public Float getHeight() {
+        return height;
+    }
+
+    public void setHeight(Float height) {
+        this.height = height;
+    }
+
+    public Float getWingspan() {
+        return wingspan;
+    }
+
+    public void setWingspan(Float wingspan) {
+        this.wingspan = wingspan;
+    }
+
 }

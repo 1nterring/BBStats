a494b7a057859c7248ed3c7ac49a381dd0cc1e81
Wisnih <97796360+Wisnih@users.noreply.github.com>
2024-05-09 22:21:57 +0200

    Update PlayerDataDTO.java
    Zrobione PlayerDataDTO

--- BBStats/src/main/java/com/uep/wap/dto/PlayerDataDTO.java
+++ BBStats/src/main/java/com/uep/wap/dto/PlayerDataDTO.java
@@ -4,5 +4,14 @@ import java.util.List;
 
 public class PlayerDataDTO {
 
+    List<PlayerDTO> players;
+
+    public List<PlayerDTO> getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(List<PlayerDTO> players) {
+        this.players = players;
+    }
 
 }

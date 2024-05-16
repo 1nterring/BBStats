81dd2f15b876b6f3df52e977c113041bf641d4f9
Wisnih <97796360+Wisnih@users.noreply.github.com>
2024-05-16 19:06:37 +0200

    Update ManagerController.java

--- BBStats/src/main/java/com/uep/wap/controller/ManagerController.java
+++ BBStats/src/main/java/com/uep/wap/controller/ManagerController.java
@@ -1,5 +1,30 @@
 package com.uep.wap.controller;
 
+import org.springframework.web.bind.annotation.*;
+
+import com.uep.wap.dto.ManagerDTO;
+import com.uep.wap.model.Managert;
+import com.uep.wap.service.ManagerService;
+
+@RestController
+@RequestMapping(path = "/api")
 public class ManagerController {
+
+    private final ManagerService managerService;
+
+    public ManagerController(ManagerService managerService) {
+        this.managerService = managerService;
+    }
+
+    @GetMapping(path = "/managers")
+    public Iterable<Manager> getAllManagers(){
+        return managerService.getAllManagers();
+    }
+
+    @PostMapping(path = "/managerAdd")
+    public String addManager(@RequestBody ManagerDTO managerDTO){
+        managerService.addManager(managerDTO);
+        return "Inserted";
+    }
 
 }

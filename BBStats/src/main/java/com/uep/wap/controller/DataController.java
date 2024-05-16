47492e32228b37976b71d64d258ebed01898be43
Wisnih <97796360+Wisnih@users.noreply.github.com>
2024-05-16 18:58:10 +0200

    Update DataController.java

--- BBStats/src/main/java/com/uep/wap/controller/DataController.java
+++ BBStats/src/main/java/com/uep/wap/controller/DataController.java
@@ -1,5 +1,29 @@
 package com.uep.wap.controller;
 
+import org.springframework.web.bind.annotation.*;
+
+import com.uep.wap.dto.DataControllerDTO;
+import com.uep.wap.model.DataController;
+import com.uep.wap.service.DataControllerService;
+
+@RestController
+@RequestMapping(path = "/api")
 public class DataController {
 
+    private final DataControllerService dataControllerService;
+
+    public DataController(DataControllerService dataControllerService) {
+        this.dataControllerService = dataControllerService;
+    }
+
+    @GetMapping(path = "/dataControllers")
+    public Iterable<DataController> getAllDataControllers(){
+        return dataControllerService.getAllDataControllers();
+    }
+
+    @PostMapping(path = "/dataControllerAdd")
+    public String addDataController(@RequestBody DataControllerDTO dataControllerDTO){
+        dataControllerService.addDataController(dataControllerDTO);
+        return "Inserted";
+    }
 }

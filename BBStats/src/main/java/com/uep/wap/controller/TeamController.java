3a9e4a1b9d359e79fd8668c7c50c5c083b9215ef
Wisnih <97796360+Wisnih@users.noreply.github.com>
2024-05-16 19:20:28 +0200

    Update TeamController.java

--- BBStats/src/main/java/com/uep/wap/controller/TeamController.java
+++ BBStats/src/main/java/com/uep/wap/controller/TeamController.java
@@ -1,5 +1,30 @@
 package com.uep.wap.controller;
 
+import org.springframework.web.bind.annotation.*;
+
+import com.uep.wap.dto.TeamDTO;
+import com.uep.wap.model.Team;
+import com.uep.wap.service.TeamService;
+
+@RestController
+@RequestMapping(path = "/api")
 public class TeamController {
+
+    private final TeamService teamService;
+
+    public TeamController(TeamService teamService) {
+        this.teamService = teamService;
+    }
+
+    @GetMapping(path = "/teams")
+    public Iterable<Team> getAllTeams(){
+        return teamService.getAllTeams();
+    }
+
+    @PostMapping(path = "/teamAdd")
+    public String addTeam(@RequestBody TeamDTO teamDTO){
+        teamService.addTeam(teamDTO);
+        return "Inserted";
+    }
 
 }

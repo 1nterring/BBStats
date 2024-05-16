41c2ef0de792fe2ca96cde97c7217406dd9c3e9b
Wisnih <97796360+Wisnih@users.noreply.github.com>
2024-05-16 18:53:13 +0200

    Update ContractController.java

--- BBStats/src/main/java/com/uep/wap/controller/ContractController.java
+++ BBStats/src/main/java/com/uep/wap/controller/ContractController.java
@@ -1,5 +1,35 @@
 package com.uep.wap.controller;
 
+import org.springframework.web.bind.annotation.*;
+
+import com.uep.wap.dto.ContractDTO;
+import com.uep.wap.model.Contract;
+import com.uep.wap.service.ContractService;
+
+@RestController
+@RequestMapping(path = "/api")
 public class ContractController {
+
+    private final ContractService contractService;
+
+    public ContractController(ContractService contractService) {
+        this.contractService = contractService;
+    }
+
+    @GetMapping(path = "/hello")
+    public String sayHello(){
+        return "Hello!";
+    }
+
+    @GetMapping(path = "/contracts")
+    public Iterable<Contract> getAllContracts(){
+        return contractService.getAllContracts();
+    }
+
+    @PostMapping(path = "/contractAdd")
+    public String addContract(@RequestBody ContractDTO contractDTO){
+        contractService.addContract(contractDTO);
+        return "Inserted";
+    }
 
 }

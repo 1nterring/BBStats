cc0327b037c5f7c1a73b974f990ea0bc3c8eb3b2
Wisnih <97796360+Wisnih@users.noreply.github.com>
2024-05-16 18:49:17 +0200

    Update AdministratorController.java

--- BBStats/src/main/java/com/uep/wap/controller/AdministratorController.java
+++ BBStats/src/main/java/com/uep/wap/controller/AdministratorController.java
@@ -1,5 +1,34 @@
 package com.uep.wap.controller;
 
+import org.springframework.web.bind.annotation.*;
+
+import com.uep.wap.dto.AdministratorDTO;
+import com.uep.wap.model.Administrator;
+import com.uep.wap.service.AdministratorService;
+
+@RestController
+@RequestMapping(path = "/api")
 public class AdministratorController {
 
+    private final AdministratorService administratorService;
+
+    public AdministratorController(AdministratorService administratorService) {
+        this.administratorService = administratorService;
+    }
+
+    @GetMapping(path = "/hello")
+    public String sayHello(){
+        return "Hello!";
+    }
+
+    @GetMapping(path = "/administrators")
+    public Iterable<Administrator> getAllAdministrators(){
+        return administratorService.getAllAdministrators();
+    }
+
+    @PostMapping(path = "/administratorAdd")
+    public String addAdministrator(@RequestBody AdministratorDTO administratorDTO){
+        administratorService.addAdministrator(administratorDTO);
+        return "Inserted";
+    }
 }

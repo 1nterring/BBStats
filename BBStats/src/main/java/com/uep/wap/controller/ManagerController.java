c9337f1cfb78d466bc4d2f7fecc035a778b057db
Wisnih <97796360+Wisnih@users.noreply.github.com>
2024-05-16 19:07:27 +0200

    Update ManagerController.java

--- BBStats/src/main/java/com/uep/wap/controller/ManagerController.java
+++ BBStats/src/main/java/com/uep/wap/controller/ManagerController.java
@@ -3,7 +3,7 @@ package com.uep.wap.controller;
 import org.springframework.web.bind.annotation.*;
 
 import com.uep.wap.dto.ManagerDTO;
-import com.uep.wap.model.Managert;
+import com.uep.wap.model.Manager;
 import com.uep.wap.service.ManagerService;
 
 @RestController

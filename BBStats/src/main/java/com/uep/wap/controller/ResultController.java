35103436ad32f8e7c1e948b96dba5a809eb61f92
Wisnih <97796360+Wisnih@users.noreply.github.com>
2024-05-16 19:09:17 +0200

    Update ResultController.java

--- BBStats/src/main/java/com/uep/wap/controller/ResultController.java
+++ BBStats/src/main/java/com/uep/wap/controller/ResultController.java
@@ -1,5 +1,30 @@
 package com.uep.wap.controller;
 
+import org.springframework.web.bind.annotation.*;
+
+import com.uep.wap.dto.ResultDTO;
+import com.uep.wap.model.Result;
+import com.uep.wap.service.ResultService;
+
+@RestController
+@RequestMapping(path = "/api")
 public class ResultController {
+
+    private final ResultService resultService;
+
+    public ResultController(ResultService resultService) {
+        this.resultService = resultService;
+    }
+
+    @GetMapping(path = "/results")
+    public Iterable<Result> getAllResults(){
+        return resultService.getAllResults();
+    }
+
+    @PostMapping(path = "/resultAdd")
+    public String addResult(@RequestBody ResultDTO resultDTO){
+        resultService.addResult(resultDTO);
+        return "Inserted";
+    }
 
 }
